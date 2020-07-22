5428ba6de20971fb499f809d19c369acdf7f47c3
jadentang <jadentang@tencent.com>
2020-07-22 18:00:55 +0800

    新增welcome页面

---
+++ app/src/main/java/com/example/administrator/share/activity/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.administrator.share.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.administrator.share.R;
+
+
+public class WelcomeActivity extends Activity {
+
+    // 声明控件对象
+//    private DataResource data;
+    private int count = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Integer time = 1000;
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, MainMenuActivity.class));
+                finish();
+            }
+        }, time);
+
+        //从必应每日一图获取图片并显示
+//        welcomeIv = findViewById(R.id.iv_welcome);
+//        data=new DataResource();
+//        data.getData();
+//        welcomeIv.setImageBitmap((Bitmap)data.getList().get(0).get("pic"));
+    }
+
+
+}
