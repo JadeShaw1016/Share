6f397e6349984d60639aefedb097b761e47eb621
jadentang <jadentang@tencent.com>
2020-07-24 16:10:18 +0800

    重构发布作品页面

--- app/src/main/java/com/example/administrator/share/activity/AddnewsActivity.java
+++ app/src/main/java/com/example/administrator/share/activity/AddnewsActivity.java
@@ -17,16 +17,22 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.administrator.share.R;
+import com.example.administrator.share.base.BaseActivity;
 
 import java.io.File;
 
-public class AddnewsActivity extends AppCompatActivity {
+public class AddnewsActivity extends BaseActivity implements View.OnClickListener{
 
     private ImageView mIv1;
     public static final int TAKE_PHOTO = 1;
@@ -34,65 +40,162 @@ public class AddnewsActivity extends AppCompatActivity {
     private ImageView mTakePhoto, mChoosePhoto;
     private ImageView picture;
     private Uri imageUri;
+    private EditText titleEt;
+    private EditText contentEt;
+    private TextView titlelLenTv;
+    private Button releaseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addnews);
+        findViewById();
+        initView();
 
-        mIv1 = (ImageView) findViewById(R.id.title_back);
-        mIv1.setOnClickListener(new View.OnClickListener() {
+        titleEt.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                //Toast.makeText(AddnewsActivity.this, "返回上个界面", Toast.LENGTH_SHORT).show();
-                finish();
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                int length =charSequence.length();
+                titlelLenTv.setText(length+"/"+12);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
+    }
+
+    @Override
+    protected void findViewById() {
+        titleEt = (EditText) findViewById(R.id.add_news_et_share_title);
+        contentEt = (EditText) findViewById(R.id.add_news_et_share_content);
+        titlelLenTv = (TextView) findViewById(R.id.add_news_tv_title_length);
+        mIv1 = (ImageView) findViewById(R.id.title_back);
         mTakePhoto = (ImageView) findViewById(R.id.add_news_iv_photo);
         mChoosePhoto = (ImageView) findViewById(R.id.choose_from_album);
         picture = (ImageView) findViewById(R.id.iv_picture);
+        releaseBtn = (Button) findViewById(R.id.add_news_btn_release);
+    }
 
-        mTakePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //创建file对象，用于存储拍照后的图片；
-                File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
+    @Override
+    protected void initView() {
+        mIv1.setOnClickListener(this);
+        mTakePhoto.setOnClickListener(this);
+        mChoosePhoto.setOnClickListener(this);
+        releaseBtn.setOnClickListener(this);
+    }
 
-                try {
-                    if (outputImage.exists()) {
-                        outputImage.delete();
-                    }
-                    outputImage.createNewFile();
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.title_back:
+                this.finish();
+                break;
+            case R.id.add_news_iv_photo:
+                takePhoto();
+                break;
+            case R.id.choose_from_album:
+                choosePhoto();
+                break;
+            case R.id.add_news_btn_release:
+                checkInfo();
+                break;
+            default:
+                break;
+        }
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+    }
 
-                if (Build.VERSION.SDK_INT >= 24) {
-                    imageUri = FileProvider.getUriForFile(AddnewsActivity.this,
-                            "com.example.administrator.ifitapp2.fileprovider", outputImage);
-                } else {
-                    imageUri = Uri.fromFile(outputImage);
-                }
+    private void takePhoto(){
+        //创建file对象，用于存储拍照后的图片；
+        File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
 
-                //启动相机程序
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, TAKE_PHOTO);
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
             }
-        });
+            outputImage.createNewFile();
 
-        mChoosePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(AddnewsActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(AddnewsActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                } else {
-                    openAlbum();
-                }
-            }
-        });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            imageUri = FileProvider.getUriForFile(AddnewsActivity.this,
+                    "com.example.administrator.ifitapp2.fileprovider", outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+
+        //启动相机程序
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    private void choosePhoto(){
+        if (ContextCompat.checkSelfPermission(AddnewsActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(AddnewsActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            openAlbum();
+        }
+    }
+
+    private void checkInfo() {
+        String titleStr = titleEt.getText().toString();
+        String contentStr = contentEt.getText().toString();
+        if (TextUtils.isEmpty(titleStr)) {
+            DisplayToast("请输入一个标题");
+            Toast.makeText(this, "请输入一个标题", Toast.LENGTH_SHORT).show();
+            titleEt.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(contentStr)) {
+            DisplayToast("请输入新鲜事");
+            contentEt.requestFocus();
+            return;
+        }
+        releaseNews();
+    }
+
+    private void releaseNews() {
+        String titleStr = titleEt.getText().toString();
+        String contentStr = contentEt.getText().toString();
+
+//        uiFlusHandler.sendEmptyMessage(DISMISS_LOADING_DIALOG);
+        String url;
+        DisplayToast("发布成功！");
+//        if (imageFile != null && imageFile.exists()) {
+//            url = Constants.BASE_URL + "News?method=releaseNewsWithImage";
+//            OkHttpUtils
+//                    .post()
+//                    .addFile("image", imageFile.getName(), imageFile)
+//                    .url(url)
+//                    .id(1)
+//                    .addHeader("content-Type", "multipart/form-data; boundary=" + UUID.randomUUID().toString())
+//                    .addParams("title", titleStr)
+//                    .addParams("content", contentStr)
+//                    .addParams("userId", Constants.USER.getUserId() + "")
+//                    .build()
+//                    .execute(new MyStringCallback());
+//        } else {
+//            url = Constants.BASE_URL + "News?method=releaseNewsWithoutImage";
+//            OkHttpUtils
+//                    .post()
+//                    .url(url)
+//                    .id(1)
+//                    .addParams("title", titleStr)
+//                    .addParams("content", contentStr)
+//                    .addParams("userId", Constants.USER.getUserId() + "")
+//                    .build()
+//                    .execute(new MyStringCallback());
+//        }
     }
 
     //打开相册
