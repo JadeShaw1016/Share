0459ccf3b6bbd663b628307f7c8786fb0dff86c8
jadentang <jadentang@tencent.com>
2020-07-24 16:14:56 +0800

    新增首页跳转逻辑

--- app/src/main/java/com/example/administrator/share/fragment/FirstPageFragment.java
+++ app/src/main/java/com/example/administrator/share/fragment/FirstPageFragment.java
@@ -1,16 +1,19 @@
 package com.example.administrator.share.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.example.administrator.share.R;
+import com.example.administrator.share.activity.DetailActivity;
 import com.example.administrator.share.adapter.ListAdapter;
 import com.example.administrator.share.util.DataResource;
 
@@ -52,6 +55,7 @@ public class FirstPageFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_firstpage, null);
         initView(view);
         initData();
+        Click();
 //        view.findViewById(R.id.iv1).setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -59,6 +63,7 @@ public class FirstPageFragment extends Fragment {
 //                startActivity(intent);
 //            }
 //        });
+
 
         //数据
         data_list = new ArrayList<String>();
@@ -85,6 +90,19 @@ public class FirstPageFragment extends Fragment {
         mAdapter=new ListAdapter(getActivity().getBaseContext(),mList,data);
         mListView.setAdapter(mAdapter);
 
+    }
+
+    private void Click(){
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                Map map = mList.get(i);
+//                Toast.makeText(getActivity(),map.get("time").toString(),
+//                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void initData(){
