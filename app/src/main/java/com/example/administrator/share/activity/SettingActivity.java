158cbe041afd3312fcf7e83747500dafe1d9f401
jadentang <jadentang@tencent.com>
2020-07-27 17:10:48 +0800

    修改继承结构，新增退出登录按钮逻辑实现

--- app/src/main/java/com/example/administrator/share/activity/SettingActivity.java
+++ app/src/main/java/com/example/administrator/share/activity/SettingActivity.java
@@ -1,30 +1,51 @@
 package com.example.administrator.share.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.administrator.share.R;
+import com.example.administrator.share.base.BaseActivity;
+import com.example.administrator.share.util.AppManager;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends BaseActivity implements View.OnClickListener {
 
-    private ImageView mIv1;
+    private ImageView backIv;
+    private ImageView exitIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
-        mIv1 = (ImageView) findViewById(R.id.title_back);
-        mIv1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(AddnewsActivity.this, "返回上个界面", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
-
+        findViewById();
+        initView();
     }
 
- }
+    @Override
+    protected void findViewById() {
+        backIv = (ImageView) findViewById(R.id.title_back);
+        exitIv = (ImageView) findViewById(R.id.me_item_exit);
+    }
+
+    @Override
+    protected void initView() {
+        backIv.setOnClickListener(this);
+        exitIv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.title_back:
+                finish();
+                break;
+            case R.id.me_item_exit:
+                SystemClock.sleep(500);
+                AppManager.getInstance().killAllActivity();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+    }
+}
