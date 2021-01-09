9660de0a8843c6a205d4f9dee1932fd8d17e48e4
JadeShaw1016 <648752148@qq.com>
2021-01-10 00:39:40 +0800

    删除主菜单界面的权限询问

--- app/src/main/java/com/example/administrator/share/activity/MainMenuActivity.java
+++ app/src/main/java/com/example/administrator/share/activity/MainMenuActivity.java
@@ -1,11 +1,8 @@
 package com.example.administrator.share.activity;//这里换成你自己的
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -40,12 +37,6 @@ public class MainMenuActivity extends BaseActivity implements View.OnClickListen
     private static final String TAG_PAGE_PERSON = "我的";
 
     private MainNavigateTabBar mNavigateTabBar;
-
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            "android.permission.READ_EXTERNAL_STORAGE",
-            "android.permission.WRITE_EXTERNAL_STORAGE" };
-
     public static Context mContext;
 
 
@@ -56,7 +47,6 @@ public class MainMenuActivity extends BaseActivity implements View.OnClickListen
         findViewById();
         mNavigateTabBar.onRestoreInstanceState(savedInstanceState);
         initView();
-        verifyStoragePermissions(this);
         mContext = this;
     }
 
@@ -153,19 +143,5 @@ public class MainMenuActivity extends BaseActivity implements View.OnClickListen
                 });
     }
 
-    public static void verifyStoragePermissions(Activity activity) {
-
-        try {
-            //检测是否有写的权限
-            int permission = ActivityCompat.checkSelfPermission(activity,
-                    "android.permission.WRITE_EXTERNAL_STORAGE");
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                // 没有写的权限，去申请写的权限，会弹出对话框
-                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
 }
