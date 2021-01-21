9c8aa4a8d2f8cc05c09641fcfc1fa520107afe48
JadeShaw1016 <648752148@qq.com>
2021-01-21 22:46:41 +0800

    新增水果首页和recyclerview

---
+++ app/src/main/java/com/example/administrator/share/fragment/FruitFragment.java
@@ -0,0 +1,194 @@
+package com.example.administrator.share.fragment;
+
+import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.administrator.share.R;
+import com.example.administrator.share.activity.MainMenuActivity;
+import com.example.administrator.share.adapter.FirstPageListAdapter;
+import com.example.administrator.share.adapter.FruitAdapter;
+import com.example.administrator.share.entity.Fruit;
+import com.example.administrator.share.util.DataResource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class FruitFragment extends Fragment implements AdapterView.OnItemClickListener{
+
+    private Spinner spinner;
+    private ListView mListView;
+    private List<Map<String,Object>> mList;
+    private DataResource data;
+    private FirstPageListAdapter mAdapter;
+    private Dialog dialog;
+
+
+    private Fruit[] fruits = {new Fruit("Apple", R.drawable.apple), new Fruit("Banana", R.drawable.banana),
+            new Fruit("Orange", R.drawable.orange), new Fruit("Watermelon", R.drawable.watermelon),
+            new Fruit("Pear", R.drawable.pear), new Fruit("Grape", R.drawable.grape),
+            new Fruit("Pineapple", R.drawable.pineapple), new Fruit("Strawberry", R.drawable.strawberry),
+            new Fruit("Cherry", R.drawable.cherry), new Fruit("Mango", R.drawable.mango)};
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
+
+    private SwipeRefreshLayout swipeRefresh;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fruit, null);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Data deleted", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(getActivity(), "Data restored", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+        initFruits();
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+        swipeRefresh = view.findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.fuxk_base_color_cyan);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+            }
+        });
+
+        findViewById(view);
+        initView();
+        setAdapter();
+        return view;
+    }
+
+    private void findViewById(View view){
+        mListView=view.findViewById(R.id.list);
+        spinner = view.findViewById(R.id.spinner);
+        mList=new ArrayList<>();
+        data=new DataResource();
+        mAdapter=new FirstPageListAdapter(getActivity().getBaseContext(),mList,data);
+        mListView.setAdapter(mAdapter);
+
+    }
+
+    private void initView(){
+        mListView.setOnItemClickListener(this);
+    }
+
+    private void initFruits() {
+        fruitList.clear();
+        for (int i = 0; i < 50; i++) {
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
+    }
+
+    private void refreshFruits() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view,int position, long id) {
+        //大图所依附的dialog
+        dialog = new Dialog(MainMenuActivity.mContext, R.style.MyDialogStyle_float_center);
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setImageBitmap((Bitmap) data.getList().get(position).get("pic"));
+        dialog.setContentView(imageView);
+        dialog.show();
+
+        //大图的点击事件（点击让他消失）
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void initData(){
+        data.getData();
+        mList.addAll(data.getList());
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void setAdapter(){
+        //数据
+        List<String> data_list = new ArrayList<>();
+        data_list.add("   我的关注   ");
+        data_list.add("   干货分享   ");
+
+        //适配器
+        ArrayAdapter<String> arr_adapter = new ArrayAdapter<>(getActivity().getBaseContext(), android.R.layout.simple_spinner_item, data_list);
+        //设置样式
+        arr_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //加载适配器
+//        spinner.getChildAt(-1).setVisibility(view.INVISIBLE);
+        spinner.setAdapter(arr_adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                if(position == 1) {
+                    initData();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
