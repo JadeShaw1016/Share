35e9cf3775886f3d1655eccd5ae48f7cf0ed39cc
JadeShaw1016 <648752148@qq.com>
2021-02-04 21:58:53 +0800

    新增上传头像功能（注册页面）

--- app/src/main/java/com/example/administrator/share/activity/RegisterActivity.java
+++ app/src/main/java/com/example/administrator/share/activity/RegisterActivity.java
@@ -1,12 +1,27 @@
 package com.example.administrator.share.activity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +37,12 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
+import com.zxy.tiny.Tiny;
+import com.zxy.tiny.callback.FileWithBitmapCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 import okhttp3.Call;
 
@@ -38,6 +59,21 @@ public class RegisterActivity extends BaseActivity implements OnClickListener {
     private EditText et_weight;
     private Button register_login;
     private RadioGroup radio_sex;
+    private ImageButton imageButton;
+
+    //调取系统摄像头的请求码
+    private static final int MY_ADD_CASE_CALL_PHONE = 6;
+    //打开相册的请求码
+    private static final int MY_ADD_CASE_CALL_PHONE2 = 7;
+    private AlertDialog.Builder builder;
+    private AlertDialog dialog;
+    private LayoutInflater inflater;
+    private View layout;
+    private TextView takePhotoTV;
+    private TextView choosePhotoTV;
+    private TextView cancelTV;
+    private File imageFile;
+
 
     private MyDialogHandler uiFlusHandler;
 
@@ -55,7 +91,7 @@ public class RegisterActivity extends BaseActivity implements OnClickListener {
         this.title_back.setOnClickListener(this);
         this.titleText.setText(TITLE_NAME);
         this.register_login.setOnClickListener(this);
-
+        imageButton.setOnClickListener(this);
         uiFlusHandler = new MyDialogHandler(mContext, "正在注册...");
 
     }
@@ -70,6 +106,7 @@ public class RegisterActivity extends BaseActivity implements OnClickListener {
         et_repassword = $(R.id.reg_et_repassword);
         et_height = $(R.id.reg_et_height);
         et_weight = $(R.id.reg_et_weight);
+        imageButton = $(R.id.register_face);
 
         this.radio_sex = (RadioGroup) findViewById(R.id.radio_sex);
         this.register_login = (Button) findViewById(R.id.reg_btn_register);
@@ -84,11 +121,71 @@ public class RegisterActivity extends BaseActivity implements OnClickListener {
             case R.id.reg_btn_register:
                 register();
                 break;
+            case R.id.register_face:
+                viewInit();
+                break;
+            case R.id.photograph:
+                //"点击了照相";
+                //  6.0之后动态申请权限 摄像头调取权限,SD卡写入权限
+                //判断是否拥有权限，true则动态申请
+                if (ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                        && ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(RegisterActivity.this,
+                            new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_ADD_CASE_CALL_PHONE);
+                } else {
+                    try {
+                        //有权限,去打开摄像头
+                        takePhoto();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                dialog.dismiss();
+                break;
+            case R.id.photo:
+                //"点击了相册";
+                //  6.0之后动态申请权限 SD卡写入权限
+                if (ContextCompat.checkSelfPermission(RegisterActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(RegisterActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_ADD_CASE_CALL_PHONE2);
+                } else {
+                    //打开相册
+                    choosePhoto();
+                }
+                dialog.dismiss();
+                break;
+            case R.id.cancel:
+                dialog.dismiss();//关闭对话框
+                break;
+            default:break;
         }
     }
 
-    private void register() {
 
+    public void viewInit() {
+
+        builder = new AlertDialog.Builder(this);//创建对话框
+        inflater = getLayoutInflater();
+        layout = inflater.inflate(R.layout.dialog_select_photo, null);//获取自定义布局
+        builder.setView(layout);//设置对话框的布局
+        dialog = builder.create();//生成最终的对话框
+        dialog.show();//显示对话框
+
+        takePhotoTV = layout.findViewById(R.id.photograph);
+        choosePhotoTV = layout.findViewById(R.id.photo);
+        cancelTV = layout.findViewById(R.id.cancel);
+        //设置监听
+        takePhotoTV.setOnClickListener(this);
+        choosePhotoTV.setOnClickListener(this);
+        cancelTV.setOnClickListener(this);
+    }
+
+
+    private void register() {
         String username = et_username.getText().toString().trim();
         String password = et_password.getText().toString().trim();
         String repassword = et_repassword.getText().toString().trim();
@@ -113,13 +210,15 @@ public class RegisterActivity extends BaseActivity implements OnClickListener {
         String url = Constants.BASE_URL + "User?method=register";
         OkHttpUtils
                 .post()
+                .addFile("face", imageFile.getName(), imageFile)
                 .url(url)
+                .id(1)
+                .addHeader("content-Type", "multipart/form-data; boundary=" + UUID.randomUUID().toString())
                 .addParams("username", username)
                 .addParams("password", password)
                 .addParams("sex", sex)
                 .addParams("height", height)
                 .addParams("weight", weight)
-                .id(1)
                 .build()
                 .execute(new MyStringCallback());
     }
@@ -132,7 +231,7 @@ public class RegisterActivity extends BaseActivity implements OnClickListener {
             switch (id) {
                 case 1:
                     uiFlusHandler.sendEmptyMessage(DISMISS_LOADING_DIALOG);
-                    User user = null;
+                    User user;
                     try {
                         user = gson.fromJson(response, User.class);
                     } catch (JsonSyntaxException e) {
@@ -164,4 +263,127 @@ public class RegisterActivity extends BaseActivity implements OnClickListener {
             DisplayToast("网络链接出错！");
         }
     }
+
+    private void takePhoto() throws IOException {
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        // 获取文件
+        File file = createFileIfNeed("UserIcon.png");
+        //拍照后原图回存入此路径下
+        Uri uri;
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+            uri = Uri.fromFile(file);
+        } else {
+            /**
+             * 7.0 调用系统相机拍照不再允许使用Uri方式，应该替换为FileProvider
+             * 并且这样可以解决MIUI系统上拍照返回size为0的情况
+             */
+            uri = FileProvider.getUriForFile(this, "com.example.administrator.share.fileprovider", file);
+        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        startActivityForResult(intent, 1);
+    }
+
+    // 在sd卡中创建一保存图片（原图和缩略图共用的）文件夹
+    private File createFileIfNeed(String fileName) throws IOException {
+        String fileA = getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/nbinpic";
+        File fileJA = new File(fileA);
+        if (!fileJA.exists()) {
+            fileJA.mkdirs();
+        }
+        File file = new File(fileA, fileName);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        return file;
+    }
+
+    /**
+     * 打开相册
+     */
+    private void choosePhoto() {
+        //这是打开系统默认的相册(就是你系统怎么分类,就怎么显示,首先展示分类列表)
+        Intent picture = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(picture, 2);
+    }
+    /**
+     * 申请权限回调方法
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_ADD_CASE_CALL_PHONE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    takePhoto();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(this,"拒绝了你的请求",Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (requestCode == MY_ADD_CASE_CALL_PHONE2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                choosePhoto();
+            } else {
+                Toast.makeText(this,"拒绝了你的请求",Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    /**
+     * startActivityForResult执行后的回调方法，接收返回的图片
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode != Activity.RESULT_CANCELED) {
+            String state = Environment.getExternalStorageState();
+            if (!state.equals(Environment.MEDIA_MOUNTED)) return;
+            // 把原图显示到界面上
+            Tiny.FileCompressOptions options = new Tiny.FileCompressOptions();
+            Tiny.getInstance().source(readpic()).asFile().withOptions(options).compress(new FileWithBitmapCallback() {
+                @Override
+                public void callback(boolean isSuccess, Bitmap bitmap, String outfile, Throwable t) {
+                    saveImageToServer(bitmap, outfile);//显示图片到imgView上
+                }
+            });
+        } else if (requestCode == 2 && resultCode == Activity.RESULT_OK
+                && null != data) {
+            try {
+                Uri selectedImage = data.getData();//获取路径
+                Tiny.FileCompressOptions options = new Tiny.FileCompressOptions();
+                Tiny.getInstance().source(selectedImage).asFile().withOptions(options).compress(new FileWithBitmapCallback() {
+                    @Override
+                    public void callback(boolean isSuccess, Bitmap bitmap, String outfile, Throwable t) {
+                        saveImageToServer(bitmap, outfile);
+                    }
+                });
+            } catch (Exception e) {
+                //"上传失败");
+            }
+        }
+    }
+    /**
+     * 从保存原图的地址读取图片
+     */
+    private String readpic() {
+        String filePath = getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/nbinpic/" + "UserIcon.png";
+        return filePath;
+    }
+    private void saveImageToServer(final Bitmap bitmap, String outfile) {
+        // 这里就可以将图片文件 file 上传到服务器,上传成功后可以将bitmap设置给你对应的图片展示
+        imageFile= new File(outfile);
+        imageButton.setImageBitmap(bitmap);
+    }
+
+
 }
