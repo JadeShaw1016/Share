948f80992cc9683b9180d02c8153aedfb54ec6ca
JadeShaw1016 <648752148@qq.com>
2021-02-07 20:06:15 +0800

    优化重复进入APP不要经过登录页面，直接进入主菜单页面

--- app/src/main/java/com/example/administrator/share/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/administrator/share/activity/WelcomeActivity.java
@@ -1,14 +1,27 @@
 package com.example.administrator.share.activity;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import com.example.administrator.share.R;
 import com.example.administrator.share.base.BaseActivity;
+import com.example.administrator.share.entity.User;
+import com.example.administrator.share.util.Constants;
+import com.example.administrator.share.util.SharedPreferencesUtils;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.Map;
+
+import okhttp3.Call;
 
 
 public class WelcomeActivity extends BaseActivity {
+
+    private Context mContext;
 
     @Override
     protected void findViewById() {
@@ -25,13 +38,14 @@ public class WelcomeActivity extends BaseActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        mContext = this;
         int time = 2000;
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
-                finish();
+                Map<String, String> map = SharedPreferencesUtils.getUserInfo(mContext);//获取用户名密码
+                checkUser(map);
             }
         }, time);
 
@@ -42,5 +56,45 @@ public class WelcomeActivity extends BaseActivity {
 //        welcomeIv.setImageBitmap((Bitmap)data.getList().get(0).get("pic"));
     }
 
-
+    /**
+     * 回显
+     */
+    private void checkUser(Map<String, String> map) {
+        final String username = map.get("username");
+        final String password = map.get("password");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String url = Constants.BASE_URL + "User?method=login";
+                OkHttpUtils
+                        .post()
+                        .url(url)
+                        .id(1)
+                        .addParams("username", username)
+                        .addParams("password", password)
+                        .build()
+                        .execute(new StringCallback() {
+                            @Override
+                            public void onResponse(String response, int id) {
+                                Gson gson = new Gson();
+                                User user = gson.fromJson(response, User.class);
+                                if (user.getUserId() == 0) {
+                                    openActivity(LoginActivity.class);
+                                    Log.d("WelcomeActivity","找不到用户ID");
+                                } else {
+                                    // 存储用户
+                                    Constants.USER = user;
+                                    openActivity(MainMenuActivity.class);
+                                    Log.d("WelcomeActivity","登录成功");
+                                }
+                                finish();
+                            }
+                            @Override
+                            public void onError(Call call, Exception e, int id) {
+                                DisplayToast("网络链接出错！");
+                            }
+                        });
+            }
+        }).start();
+    }
 }
