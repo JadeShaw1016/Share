8767a671dbab35b407a223a5416090cec88cc498
JadeShaw1016 <648752148@qq.com>
2021-02-09 17:13:22 +0800

    实现点赞、收藏图片显示

--- app/src/main/java/com/example/administrator/share/adapter/CollectionListAdapter.java
+++ app/src/main/java/com/example/administrator/share/adapter/CollectionListAdapter.java
@@ -2,19 +2,27 @@ package com.example.administrator.share.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.administrator.share.R;
 import com.example.administrator.share.activity.CircleDetailActivity;
 import com.example.administrator.share.entity.NewsListItem;
+import com.example.administrator.share.util.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.BitmapCallback;
 
 import java.util.List;
+
+import okhttp3.Call;
 
 /**
  * Created by djzhao on 17/05/04.
@@ -29,11 +37,13 @@ public class CollectionListAdapter extends RecyclerView.Adapter<CollectionListAd
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView title;
         TextView username;
+        ImageView imageView;
         LinearLayout mycollectionLl;
         public ViewHolder(View view) {
             super(view);
             title = view.findViewById(R.id.item_normal_title);
             username = view.findViewById(R.id.item_normal_username);
+            imageView = view.findViewById(R.id.iv_mycollection);
             mycollectionLl = view.findViewById(R.id.ll_mycollection);
         }
     }
@@ -70,6 +80,7 @@ public class CollectionListAdapter extends RecyclerView.Adapter<CollectionListAd
         NewsListItem detail = mList.get(position);
         holder.title.setText(detail.getTitle());
         holder.username.setText("作者："+detail.getUsername());
+        getImage(detail.getImage(),holder);
     }
 
     @Override
@@ -77,4 +88,27 @@ public class CollectionListAdapter extends RecyclerView.Adapter<CollectionListAd
         return mList.size();
     }
 
+    private void getImage(final String imageName, final ViewHolder holder) {
+        new AsyncTask<Void, Void, Integer>(){
+            @Override
+            protected Integer doInBackground(Void... voids) {
+                String url = Constants.BASE_URL + "Download?method=getNewsImage";
+                OkHttpUtils
+                        .get()//
+                        .url(url)//
+                        .addParams("imageName", imageName)
+                        .build()//
+                        .execute(new BitmapCallback() {
+                            @Override
+                            public void onError(Call call, Exception e, int i) {
+                            }
+                            @Override
+                            public void onResponse(Bitmap bitmap, int i) {
+                                holder.imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                return 0;
+            }
+        }.execute();
+    }
 }
